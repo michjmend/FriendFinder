00d3959248e128b27b3f8adb7497d1447037d06c
Peanut <michjmend@gmail.com>
2019-05-21 21:52:10 -0400

    removed path.join

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Expose the public directory to access CSS files
-app.use(express.static(path.join(__dirname, './app/public')));
+app.use(express.static(__dirname + '/app/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Determine the user's most compatible friend using the following as a guide:
@@ -23,14 +23,3 @@ require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
-// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-// Example:
-// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-// User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-// Total Difference: 2 + 1 + 2 = 5
-// Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
-// The closest match will be the user with the least amount of difference.
-// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-
-// The modal should display both the name and picture of the closest match.
